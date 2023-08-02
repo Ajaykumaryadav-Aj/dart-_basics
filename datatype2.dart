a22067324b38341451ac9fb9b0c7cecfcc7b1243
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2023-08-03 01:49:48 +0530

    datatype

---
+++ datatype2.dart
@@ -0,0 +1,38 @@
+void main()
+{
+    String str = 'rohan';
+    String str1 = 'sohan';
+
+   // declare boolean value
+    bool val = (str == str1);
+
+    //print the boolean value
+    print(val);
+
+    // another example boolean value
+     int a = 100;
+     int b = 75;
+
+     //declare boolean value
+     bool value = a > b;
+
+     // print the boolean value
+     print(value);
+
+     //declare a dynamic type and assign a value
+    dynamic aj = 45;
+    print(aj);
+    // reassigning value to aj
+    aj = 'ajay';
+    print(aj);
+    //reassigning value ro aj 
+    aj = 4.5;
+    print(aj);
+
+    print(str.runtimeType);
+
+
+
+    
+
+}
