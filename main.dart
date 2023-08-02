5253aeb2e24a914a380cc7105e54510da5ffccc5
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2023-08-02 19:12:14 +0530

    second commit

---
+++ main.dart
@@ -0,0 +1,8 @@
+// Importing built-in library
+    import 'dart:math';
+void main()
+{
+    
+    print("square root of 16 is: ${sqrt(16)}");
+}
+   
---
+++ main1.dart
@@ -0,0 +1,5 @@
+void main()
+{
+    int value = 10;
+    print(value);
+}git 
