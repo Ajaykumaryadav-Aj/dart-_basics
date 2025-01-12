ce95c7fb3163c0be0918b2cdea05257f442358f8
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2025-01-13 01:17:46 +0530

    creating a method for factorial

--- Question/factorial/factorial.dart
+++ Question/factorial/factorial.dart
@@ -1,8 +1,20 @@
 void main() {
-  int n = 10, fact =1;
+  int n = 10, fact = 1;
 
   for (int i = 1; i <= n; i++) {
     fact = fact * i;
   }
   print(fact);
+  print(factorial(5));
+}
+
+// create a method for factorial
+
+factorial(int n) {
+  int fact = 1;
+
+  for (int i = n; i <=n; i++) {
+    fact = fact * i;
+  }
+  return fact;
 }
