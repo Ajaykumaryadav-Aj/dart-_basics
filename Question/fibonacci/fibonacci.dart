5f1aba7e8760a50ae61280aad82698e1482ac1eb
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2025-01-18 01:05:33 +0530

    fibonacci

--- Question/fibonacci/fibonacci.dart
+++ Question/fibonacci/fibonacci.dart
@@ -7,6 +7,19 @@ void main() {
     nextnum = firstnum + secondnum;
     firstnum = secondnum;
     secondnum = nextnum;
-
   }
+  // print(fibonaci());
+}
+
+fibonaci() {
+  int firstnum = 1;
+  int secondnum = 2;
+  int nextnum;
+  for (int i = 0; i <= 10; i++) {
+    print(firstnum);
+    nextnum = firstnum + secondnum;
+    firstnum = secondnum;
+    secondnum = nextnum;
+  }
+  
 }
