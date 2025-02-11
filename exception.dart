da85007166ce20cdbe5ab949dbfe019f5d8ac09c
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2025-02-12 00:48:34 +0530

    few practice

--- exception.dart
+++ exception.dart
@@ -56,4 +56,14 @@ void main() {
   } else {
     print('not found');
   }
+
+  print(revString("Hello Bhai"));
+}
+
+revString(String input) {
+  String reversed = '';
+  for (int i = input.length - 1; i >=0; i--) {
+    reversed += input[i];
+  }
+  return reversed;
 }
