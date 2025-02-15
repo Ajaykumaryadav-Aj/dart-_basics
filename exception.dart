26631d5794cb77e86a43054c2498a9951d13c8c5
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2025-02-15 23:59:00 +0530

    practice

--- exception.dart
+++ exception.dart
@@ -36,6 +36,26 @@
 // Convert the set to a list
 
 void main() {
+
+
+List numbers = [2, 5, 7, 9, 6, 55, 33];
+
+  int max = numbers[0];
+  int min = numbers[0];
+
+  for (int i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    } else if (numbers[i] < min) {
+      min = numbers[i];
+    }
+  }
+  print(max);
+  print(min);
+
+
+
+
   var letters = <String>{'h', 'e', 'l', 'o'};
 
   letters.add('a'); //Add the letter "a" to the set
@@ -66,4 +86,13 @@ revString(String input) {
     reversed += input[i];
   }
   return reversed;
+
+
+
+
+  
+
 }
+
+
+
