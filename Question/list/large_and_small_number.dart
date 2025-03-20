14c78ba748aae5af60ee71e69fae137913a2abf4
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2025-03-20 23:29:03 +0530

    sum of all element of list

--- Question/list/large_and_small_number.dart
+++ Question/list/large_and_small_number.dart
@@ -1,18 +1,30 @@
 // Find the largest number and smallest number in List....
 
+// void main() {
+//   List numbers = [2, 5, 7, 9, 6, 55, 33];
+
+//   int max = numbers[0];
+//   int min = numbers[0];
+
+//   for (int i = 1; i < numbers.length; i++) {
+//     if (numbers[i] > max) {
+//       max = numbers[i];
+//     } else if (numbers[i] < min) {
+//       min = numbers[i];
+//     }
+//   }
+//   print(max);
+//   print(min);
+// }
+
+// Sum of all element of list *********************
+
 void main() {
-  List numbers = [2, 5, 7, 9, 6, 55, 33];
+  List arr = [1, 2, 3, 4, 5];
+  int sum = 0;
 
-  int max = numbers[0];
-  int min = numbers[0];
-
-  for (int i = 1; i < numbers.length; i++) {
-    if (numbers[i] > max) {
-      max = numbers[i];
-    } else if (numbers[i] < min) {
-      min = numbers[i];
-    }
+  for (int i = 0; i <= arr.length; i++) {
+    sum = sum + i;
   }
-  print(max);
-  print(min);
+  print(sum);
 }
