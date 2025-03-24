d0eecf0e1db68a6f96741c66c684628fc7ed3615
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2025-03-24 23:54:03 +0530

    sum of all element of array

--- Question/list/large_and_small_number.dart
+++ Question/list/large_and_small_number.dart
@@ -20,11 +20,11 @@
 // Sum of all element of list *********************
 
 void main() {
-  List arr = [1, 2, 3, 4, 5];
+  List<int> arr = [1, 2, 18, 4, 5];
   int sum = 0;
 
-  for (int i = 0; i <= arr.length; i++) {
-    sum = sum + i;
+  for (int i = 0; i < arr.length; i++) {
+    sum += arr[i];
   }
   print(sum);
 }
