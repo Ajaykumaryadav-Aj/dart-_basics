43cc77f68c70cddf04c28ad856be40ec30794047
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2025-08-29 22:57:19 +0530

    remove duplicate element

--- Question/list/frequency.dart
+++ Question/list/frequency.dart
@@ -10,4 +10,26 @@ void main() {
     }
   }
   print(freq);
+  removeDuplicate();
+}
+
+// Remove duplicate element in list
+
+removeDuplicate() {
+  List<int> numbers = [
+    1,
+    2,
+    3,
+    4,
+    3,
+    1,
+    2,
+  ];
+  List<int> unique = [];
+  for (var i = 0; i < numbers.length; i++) {
+    if (unique.contains(numbers[i]) == false) {
+      unique.add(numbers[i]);
+    }
+  }
+  print(unique);
 }
