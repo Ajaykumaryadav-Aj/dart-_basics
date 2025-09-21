3c68d71ceb6e3a6a32368462eb814687068c569c
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2025-09-21 23:58:02 +0530

    ..

--- Question/list/reversedList.dart
+++ Question/list/reversedList.dart
@@ -1,16 +1,26 @@
 void main() {
   // Given input list
-  List<int> numbers = [1, 2, 3, 4, 5];
+  // List<int> numbers = [1, 2, 3, 4, 5];
 
-  print('My List: $numbers');
+  // print('My List: $numbers');
 
-  // New list to store reversed elements
-  List<int> reversedList = [];
+  // // New list to store reversed elements
+  // List<int> reversedList = [];
 
-  // Reversing the list using a for loop
-  for (int i = numbers.length - 1; i >= 0; i--) {
-    reversedList.add(numbers[i]);
+  // // Reversing the list using a for loop
+  // for (int i = numbers.length - 1; i >= 0; i--) {
+  //   reversedList.add(numbers[i]);
+  // }
+
+  // print('Reversed: $reversedList');
+  reversedLists();
+}
+
+reversedLists() {
+  List number = [1, 2, 3, 4, 5, 6, 7];
+  List reverseList = [];
+  for (int i = number.length - 1; i >= 0; i--) {
+    reverseList.add(number[i]);
   }
-
-  print('Reversed: $reversedList');
+  print(reverseList);
 }
